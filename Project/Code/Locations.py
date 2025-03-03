61700745ecba09f8ce12c7d8025279bbff2a67d5
Nathan <nathanmerz24@gmail.com>
2025-03-03 09:38:41 -0600

    Update Locations.py

--- Project/Code/Locations.py
+++ Project/Code/Locations.py
@@ -8,19 +8,20 @@ class Locations:
         self.set_location_name(location_name)
         self.set_location_description(location_description)
 
-    #helpers
-
+    #getters
     def get_location_name(self):
         return self.__location_name
 
     def get_location_description(self):
         return self.__location_description
 
+    #setters
     def set_location_name(self, location_name):
         self.__location_name = location_name
 
     def set_location_description(self, location_description):
         self.__location_description = location_description
 
+    #tostring
     def __str__(self):
         return "You are at " + str(self.get_location_name()) + str(self.get_location_description())
