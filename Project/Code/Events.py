e85df4c098c38bb186c4c1738e20f2014f989d42
Braxton Rider <rider.braxton@gmail.com>
2025-03-03 11:56:42 -0600

    Added the beginning of Events

--- Project/Code/Events.py
+++ Project/Code/Events.py
@@ -1,1 +1,14 @@
+#from Enemies import Enemies
+from Weapons import Weapons
+
+
+#This is just for testing. This is how I was able to read the file
+temp_list = []
+with open("Project/Code/Weapons/weapons.txt", "r") as file:
+    for line in file:
+        temp_list.append(line)
+        
+print(temp_list[0])
+
+w1 = Weapons("Knife", 10, "Pointy")
 
